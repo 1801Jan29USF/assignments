02952a64b4b8dd75b4a7d78f1567dbefa6f8a8c9
mattmark3 <jmark@mail.usf.edu>
2018-02-02 00:04:32 -0500

    Add files via upload

---
+++ src/Q11/Q11.java
@@ -0,0 +1,13 @@
+package Q11;
+
+import Q11_2.Floats;
+
+public class Q11 {
+	
+	//gets floats from Floats
+	public static void main(String[] args) {
+		
+		Floats f = new Floats();
+		System.out.println(f.getA() + ", " + f.getB());
+	}
+}
