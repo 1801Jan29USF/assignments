8610794f7429eb3387bb84d47c70fb083025e1a3
Jason C. Morgan <jcmorgan1000@gmail.com>
2018-02-03 00:01:13 -0500

    Update PrintTriangle.java

--- question13/PrintTriangle.java
+++ question13/PrintTriangle.java
@@ -3,8 +3,10 @@ package question13;
 public class PrintTriangle {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 4; i++) {
-			
+		
+		String [] st = {"   0","  0 1", " 1 0 1", "0 1 0 1"};
+		for(int i = 0; i < st.length; i++) {
+			System.out.println(st[i]);
 		}
 	}
 
