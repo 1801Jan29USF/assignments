75f0407a6e6a85b7960642ed58346c04d7d0e22c
unknown <iperfitt@umich.edu>
2018-02-16 20:32:49 -0800

    hw3 update

--- HW3/homework.js
+++ HW3/homework.js
@@ -92,6 +92,64 @@ function substring(someStr, length, offset) {
     return temp.join("");
 }
 
+// 6. Even Number
+// Define function: isEven(someNum)
+// Return true if even, false if odd.
+// Do not use % operator.
+function isEven(someNum) {
+
+    if (!Number.isInteger(someNum)) {
+        console.log("Please enter an integer weisenheimer!")
+        return;
+    }
+    // Check least significant rightmost bit 
+    return (someNum & 1) ? false : true;
+}
+
+// 7. Palindrome
+// Define function isPalindrome(someStr)
+// Return true if someStr is a palindrome, otherwise return false
+function isPalindrome(someStr) {
+
+    // /..../g is a global regex which operates on multiple matches in the string
+    //  [...] creates a character set. It basically matches any single character within the list
+    // \W_ match the inverse of word characters and underscores
+    let re = /[\W_]/g;
+    //get rid of non alpha-numeric terms
+    let lowRegStr = someStr.toLowerCase().replace(re, '');
+    //split string, reverse, and join back into string
+    let reverseSomeStr = lowRegStr.split('').reverse().join('');
+    return reverseSomeStr === lowRegStr;
+
+}
+
+// 9. Object literal
+// Define function traverseObject(someObj)
+// Print every property and it's value.
+function traverseObject(someObj) {
+    let propValue;
+
+    for (let propName in someObj) {
+        propValue = someObj[propName]
+
+        console.log(propName + " " + propValue);
+    }
+
+}
+
+// 10. Delete Element
+// Define function deleteElement(someArr)
+// Print length
+// Delete the third element in the array.
+// Print length
+// The lengths should be the same.
+function deleteElement(someArr) {
+    console.log(someArr.length);
+    delete someArr[2];
+    console.log(someArr.length);
+
+}
+
 //PART 2
 // 1. USA
 // Define function getUSA()
