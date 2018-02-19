6df3d57f8ee7c0e30b92cef4af0c7af0e2495163
Bobby M <31968082+sparkzy@users.noreply.github.com>
2018-02-19 10:33:35 -0500

    updated hw2

--- hw2 - JavaScript/Part 2/q12-walk-the-DOM.js
+++ hw2 - JavaScript/Part 2/q12-walk-the-DOM.js
@@ -1,5 +1,10 @@
 function walkTheDOM (node, func) {
-    return walkTheDOM(node.childNodes, walkTheDOM);
+    if (node.childNodes !== null) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            console.log(node.childNodes[i]);
+            walkTheDOM(node.childNodes[i], walkTheDOM);
+        }
+    }
 }
 
 walkTheDOM(document.documentElement, walkTheDOM);
